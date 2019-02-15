dddb054f029cb18eb18ade7eb0c57e6257f88689
Shivanshu Singh <shivnshus@gmail.com>
2019-02-15 20:13:12 +0530

    Add support for switch

---
+++ test/parser/switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  switch i {
+      case 1: {
+        fmt.Println("one")
+      }
+      case 2: {
+        fmt.Println("two")
+      }
+      case 3: {
+        fmt.Println("three")
+      }
+    }
+}
