aecf1f2a9140b8ca54b593d9937a6d4cbdd2203b
Shivanshu Singh <shivnshus@gmail.com>
2019-02-15 19:44:24 +0530

    Add dict to dot script

--- src/ASTGenerator.py
+++ src/ASTGenerator.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python3
 import argparse
+import sys
+from graphviz import Digraph
 from parser import go_parser
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--out", help="Output dot script")
 arg_parser.add_argument("input", nargs="*")
-
 args = arg_parser.parse_args()
 
+dot = Digraph()
+
+def dict_to_dot(inp):
+    global dot
+    if 'id' not in inp:
+        return
+    dot.node(inp['id'], inp['label'])
+    for child in inp['children']:
+        if child == None:
+            continue
+        if 'id' not in child:
+            continue
+        dot.edge(inp['id'], child['id'])
+        dict_to_dot(child)
+
+if (len(args.input) == 0):
+    print("Please specify input code")
+    sys.exit(0)
+
 input_file = open(args.input[0], "r")
-output_file = args.out
 
 input_data = input_file.read()
+dict_result = go_parser.parse(input_data)
 
-print(go_parser.parse(input_data))
+print(dict_result)
+
+if (args.out == None):
+    sys.exit(0)
+
+output_file = args.out
+output_handler = open(output_file, "w")
+
+dict_to_dot(dict_result)
+
+output_handler.write(dot.source)
+output_handler.close()
+print(dot.source)
---
+++ test/parser/conditionals.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+  var x = 2
+  if x != 2 {
+    var y = 3
+  } else if x != 3 {
+
+  } else {
+
+  }
+}
